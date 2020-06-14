306a281e11c2f45f5dc603720f03460198ca0994
Alexander Pavlenko <pava@bk.ru>
2020-06-14 19:47:37 +0700

    Afisha's manager tests added

--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -1,44 +1,73 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.netology.domain.AfishaItem;
+import ru.netology.domain.AfishaRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class AfishaManagerTest {
-/*
-    @Test
-    public void shouldAdd() {
-        AfishaManager manager = new AfishaManager();
-        AfishaItem movie01 = new AfishaItem(1, "http://zxc.ru/01", "Movie 01", "First genre");
-        AfishaItem movie02 = new AfishaItem(2, "http://zxc.ru/02", "Movie 02", "First genre");
-        AfishaItem movie03 = new AfishaItem(3, "http://zxc.ru/03", "Movie 03", "First genre");
+    @Mock
+    private AfishaRepository repository;
+    @InjectMocks
+    private AfishaManager manager;
+    private AfishaItem movie01 = new AfishaItem(1, "http://zxc.ru/01", "Movie 01", "First genre");
+    private AfishaItem movie02 = new AfishaItem(2, "http://zxc.ru/02", "Movie 02", "First genre");
+    private AfishaItem movie03 = new AfishaItem(3, "http://zxc.ru/03", "Movie 03", "First genre");
+    private AfishaItem movie04 = new AfishaItem(4, "http://zxc.ru/04", "Movie 04", "Second genre");
+    private AfishaItem movie05 = new AfishaItem(5, "http://zxc.ru/05", "Movie 05", "Second genre");
+    private AfishaItem movie06 = new AfishaItem(6, "http://zxc.ru/06", "Movie 06", "Second genre");
+    private AfishaItem movie07 = new AfishaItem(7, "http://zxc.ru/07", "Movie 07", "Second genre");
+    private AfishaItem movie08 = new AfishaItem(8, "http://zxc.ru/08", "Movie 08", "First genre");
+    private AfishaItem movie09 = new AfishaItem(9, "http://zxc.ru/09", "Movie 09", "Third genre");
+    private AfishaItem movie10 = new AfishaItem(10, "http://zxc.ru/10", "Movie 10", "Third genre");
+    private AfishaItem movie11 = new AfishaItem(11, "http://zxc.ru/11", "First 11", "Third genre");
+
+    @BeforeEach
+    public void setUp() {
+        //Заглушка
+        doNothing().when(repository).save(movie01);
+        doNothing().when(repository).save(movie02);
+        doNothing().when(repository).save(movie03);
+        doNothing().when(repository).save(movie04);
+
         manager.add(movie01);
         manager.add(movie02);
         manager.add(movie03);
+        manager.add(movie04);
+    }
 
+    @Test
+    public void shouldAdd() {
+        // Заглушка
+        AfishaItem[] returned = new AfishaItem[]{movie01, movie02, movie03, movie04, movie05};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).save(movie05);
+
+        manager.add(movie05);
         AfishaItem[] actual = manager.getAfishaList();
-        AfishaItem[] expected = new AfishaItem[]{movie03, movie02, movie01};
+        AfishaItem[] expected = new AfishaItem[]{movie05, movie04, movie03, movie02, movie01};
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void shouldGetLastFive() {
-        AfishaManager manager = new AfishaManager(5);
-        AfishaItem movie01 = new AfishaItem(1, "http://zxc.ru/01", "Movie 01", "First genre");
-        AfishaItem movie02 = new AfishaItem(2, "http://zxc.ru/02", "Movie 02", "First genre");
-        AfishaItem movie03 = new AfishaItem(3, "http://zxc.ru/03", "Movie 03", "First genre");
-        AfishaItem movie04 = new AfishaItem(4, "http://zxc.ru/04", "Movie 04", "Second genre");
-        AfishaItem movie05 = new AfishaItem(5, "http://zxc.ru/05", "Movie 05", "Second genre");
-        AfishaItem movie06 = new AfishaItem(6, "http://zxc.ru/06", "Movie 06", "Second genre");
-        manager.add(movie01);
-        manager.add(movie02);
-        manager.add(movie03);
-        manager.add(movie04);
+        // Заглушка
+        AfishaItem[] returned = new AfishaItem[]{movie02, movie03, movie04, movie05, movie06};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).save(movie05);
+        doNothing().when(repository).save(movie06);
+
         manager.add(movie05);
         manager.add(movie06);
-
         AfishaItem[] actual = manager.getAfishaList();
         AfishaItem[] expected = new AfishaItem[]{movie06, movie05, movie04, movie03, movie02};
 
@@ -47,20 +76,14 @@ class AfishaManagerTest {
 
     @Test
     public void shouldGetAllExists() {
-        // Количество возвращаемых фильмов, по-умолчанию, 10
-        AfishaManager manager = new AfishaManager();
-        AfishaItem movie01 = new AfishaItem(1, "http://zxc.ru/01", "Movie 01", "First genre");
-        AfishaItem movie02 = new AfishaItem(2, "http://zxc.ru/02", "Movie 02", "First genre");
-        AfishaItem movie03 = new AfishaItem(3, "http://zxc.ru/03", "Movie 03", "First genre");
-        AfishaItem movie04 = new AfishaItem(4, "http://zxc.ru/04", "Movie 04", "Second genre");
-        AfishaItem movie05 = new AfishaItem(5, "http://zxc.ru/05", "Movie 05", "Second genre");
+        // Заглушка
+        AfishaItem[] returned = new AfishaItem[]{movie01, movie02, movie03, movie04, movie05};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).save(movie05);
+        doNothing().when(repository).save(movie06);
 
-        manager.add(movie01);
-        manager.add(movie02);
-        manager.add(movie03);
-        manager.add(movie04);
         manager.add(movie05);
-
+        manager.add(movie06);
         AfishaItem[] actual = manager.getAfishaList();
         AfishaItem[] expected = new AfishaItem[]{movie05, movie04, movie03, movie02, movie01};
 
@@ -69,41 +92,33 @@ class AfishaManagerTest {
 
     @Test
     public void shouldRemoveIfExists() {
-        AfishaManager manager = new AfishaManager();
-        int idToRemove = 1;
-        AfishaItem movie01 = new AfishaItem(1, "http://zxc.ru/01", "Movie 01", "First genre");
-        AfishaItem movie02 = new AfishaItem(2, "http://zxc.ru/02", "Movie 02", "First genre");
-        AfishaItem movie03 = new AfishaItem(3, "http://zxc.ru/03", "Movie 03", "First genre");
-        manager.add(movie01);
-        manager.add(movie02);
-        manager.add(movie03);
+        int idToRemove = 3;
+        // Заглушка
+        AfishaItem[] returned = new AfishaItem[]{movie01, movie02, movie04, movie05};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).save(movie05);
+        doNothing().when(repository).removeById(idToRemove);
 
+        manager.add(movie05);
         manager.removeById(idToRemove);
-
         AfishaItem[] actual = manager.getAfishaList();
-        AfishaItem[] expected = new AfishaItem[]{movie03, movie02};
+        AfishaItem[] expected = new AfishaItem[]{movie05, movie04, movie02, movie01};
 
         assertArrayEquals(expected, actual);
     }
 
-//    @Test
+    @Test
     public void shouldNotRemoveIfNotExists() {
-        AfishaManager manager = new AfishaManager();
-        int idToRemove = 4;
-        AfishaItem movie01 = new AfishaItem(1, "http://zxc.ru/01", "Movie 01", "First genre");
-        AfishaItem movie02 = new AfishaItem(2, "http://zxc.ru/02", "Movie 02", "First genre");
-        AfishaItem movie03 = new AfishaItem(3, "http://zxc.ru/03", "Movie 03", "First genre");
-        manager.add(movie01);
-        manager.add(movie02);
-        manager.add(movie03);
+        int idToRemove = 5;
+        // Заглушка
+        AfishaItem[] returned = new AfishaItem[]{movie01, movie02, movie03, movie04};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).removeById(idToRemove);
 
         manager.removeById(idToRemove);
-
         AfishaItem[] actual = manager.getAfishaList();
-        AfishaItem[] expected = new AfishaItem[]{movie03, movie02, movie01};
+        AfishaItem[] expected = new AfishaItem[]{movie04, movie03, movie02, movie01};
 
         assertArrayEquals(expected, actual);
     }
-
- */
 }
